2ca1bdc7c2cd3461b28758e8a04082fed3a28c64
Ehryk <rhaistlin@gmail.com>
2011-11-24 22:01:04 -0900

    Adding missing files

---
+++ WebApplication/App_Code/BasePage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web.UI;
+using GPX;
+
+namespace WebApplication.App_Code
+{
+    public class BasePage : Page
+    {
+        protected string FilePath
+        {
+            get { return Session["FilePath"] as string; }
+            set { Session["FilePath"] = value; }
+        }
+
+        protected GPXFile GPX
+        {
+            get { return Session["GPX"] as GPXFile; }
+            set { Session["GPX"] = value; }
+        }
+    }
+}
---
+++ WebApplication/Upload.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using GPX;
+using WebApplication.App_Code;
+
+namespace WebApplication
+{
+    public partial class _Upload : BasePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ScriptManager.GetCurrent(this).RegisterPostBackControl(btnUpload);
+
+            if (!IsPostBack)
+                BindFiles();
+
+            lblStatus.Text = "Upload Status:";
+        }
+
+        protected void BindFiles()
+        {
+            List<FileResult> files = new List<FileResult>();
+
+            int i = 1;
+            foreach (string file in Directory.EnumerateFiles(Server.MapPath("~/Uploads/")))
+            {
+                FileInfo info = new FileInfo(file);
+                files.Add(new FileResult { Name = Path.GetFileName(file), Path = file, Number = i, Size = info.Length, Uploaded = info.CreationTime });
+                i++;
+            }
+
+            gvFiles.DataSource = files;
+            gvFiles.DataBind();
+        }
+
+        protected void UploadClicked(object sender, EventArgs e)
+        {
+            if (fileUpload.FileName != string.Empty)
+            {
+                try
+                {
+                    string filename = Path.GetFileName(fileUpload.FileName);
+
+                    if (fileUpload.FileName.ToUpper().EndsWith(".GPX"))
+                    {
+                        FilePath = Server.MapPath("~/Uploads/") + filename;
+                        fileUpload.SaveAs(FilePath);
+                        lblStatus.Text = "Upload status: File uploaded!";
+
+                        BindFiles();
+                    }
+                    else
+                    {
+                        lblStatus.Text = "Upload status: Only .gpx files are accepted.";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    lblStatus.Text = "Upload status: The file could not be uploaded. The following error occured: " + ex.Message;
+                }
+            }
+            else
+            {
+                lblStatus.Text = "Upload status: No file selected";
+            }
+        }
+    }
+}
